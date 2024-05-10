44f6417edd6baa22deb367cc0902bd922896afb0
FacuAlm <facundoalmaraz8@gmail.com>
2024-05-10 14:42:43 -0300

    fix

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ import PatientRoutes from "./routes/patientsRoutes.js";
 import UserRoutes from "./routes/userRoutes.js";
 import cors from "cors";
 
-
 const app = express();
 app.use(express.json());
 dotenv.config();
 
 connectDB();
 
+const CorsOptions = {
+  origin: function (origin, callback) {
+    const whitelist = ["https://fono-quiz-frontend.vercel.app"];
 
-const corsOptions = {
-  origin: "https://fono-quiz-frontend.vercel.app",
-  credentials: true,
-  optionSuccessStatus: 200,
+    if (process.argv[2] === "--api") {
+      whitelist.push(undefined);
+    }
+
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
+    }
+  },
 };
 
-app.use(cors(corsOptions));
+app.use(cors(CorsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -28,7 +36,6 @@ app.get("/", (req, res) => {
 //routes
 app.use("/api/users", UserRoutes);
 app.use("/api/patients", PatientRoutes);
-
 
 const PORT = process.env.PORT || 4000;
 
