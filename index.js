9bd5081fec8d776e67ce83f28d0a47a69bb7dafa
FacuAlm <facundoalmaraz8@gmail.com>
2024-05-10 03:34:06 -0300

    cambios

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-import server from "./server.js";
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import PatientRoutes from "./routes/Patient.Routes.js";
+import UserRoutes from "./routes/User.Routes.js";
+import cors from "cors";
 
-const port = process.env.PORT || 4000;
+dotenv.config();
 
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//routes
+app.use("/api/users", UserRoutes);
+app.use("/api/patients", PatientRoutes);
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
